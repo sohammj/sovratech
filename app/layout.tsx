f0da7dd8217eed777dff76755e8e35c349dadd4f
Sohammj <soham0144@gmail.com>
2025-10-31 23:45:48 +0530

    ss

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,15 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "SovraTech",
   description: "Empowering Your Digital Presence",
+  icons: {
+    icon: [
+      { url: "/favicon.svg", type: "image/svg+xml" },
+      { url: "/favicon-32x32.png", sizes: "32x32" },
+      { url: "/favicon-16x16.png", sizes: "16x16" },
+    ],
+    apple: "/favicon-32x32.png",
+  },
+
 };
 
 export default function RootLayout({
